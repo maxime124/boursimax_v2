0fb57986cf83508782a63d40e0e656df8225c863
xouxou <xouxou_coursera@hotmail.com>
2017-02-10 11:28:26 +0100

    - first use of service

--- src/app/epargne/epargne.component.ts
+++ src/app/epargne/epargne.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Renderer,AfterViewInit } from '@angular/core';
 import { ActivatedRoute, Params }   from '@angular/router';
 import {Account} from "../class/account";
 import { EpargneService } from './epargne.service';
+import * as echarts from 'echarts';
 
 @Component({
   selector: 'app-epargne',
@@ -9,21 +10,48 @@ import { EpargneService } from './epargne.service';
   styleUrls: ['./epargne.component.css'],
   providers:      [EpargneService]
 })
-export class EpargneComponent implements OnInit {
+
+export class EpargneComponent implements OnInit,AfterViewInit {
   accounts: Account[];
   private hiddenForm: boolean;
 
   constructor(
       private route:          ActivatedRoute,
-      private epargneService: EpargneService
+      private epargneService: EpargneService,
+      private renderer : Renderer
   ) {
     this.hiddenForm = true;
+
   }
 
   ngOnInit(): void {
     this.epargneService.getAccounts().then(
         accounts => this.accounts = accounts
     );
+  }
+
+  ngAfterViewInit() {
+    const selectDom = this.renderer.selectRootElement('#test_chart');
+    var options = {
+      title: {
+        text: 'ECharts entry example'
+      },
+      tooltip: {},
+      legend: {
+        data:['Sales']
+      },
+      xAxis: {
+        data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
+      },
+      yAxis: {},
+      series: [{
+        name: 'Sales',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    };
+    var chartView = echarts.init(selectDom);
+    chartView.setOption(options);
   }
 
   updateListeAccount() {
